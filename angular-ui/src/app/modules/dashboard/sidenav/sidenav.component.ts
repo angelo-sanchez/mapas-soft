8072de715ca2fe08cf757bcc6e504a7452d4725f
joaquingonzlz <50594840+joaquingonzlz@users.noreply.github.com>
2021-08-08 16:37:58 -0300

    Arreglo de interfaz

--- angular-ui/src/app/modules/dashboard/sidenav/sidenav.component.ts
+++ angular-ui/src/app/modules/dashboard/sidenav/sidenav.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
-  public lista: string[] = ['Mi unidad', 'Configuracion'];
+  public listItemSelected : string = 'Mapas';
+
+
   
   constructor() { }
 
@@ -14,3 +15,4 @@ export class SidenavComponent implements OnInit {
   }
 
 }
+
