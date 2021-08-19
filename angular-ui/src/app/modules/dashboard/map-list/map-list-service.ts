a838a9532ac61c5b5930029638718830ef26e6e2
joaquingonzlz <50594840+joaquingonzlz@users.noreply.github.com>
2021-08-18 23:53:43 -0300

    Ajustar estilos vista-mapa

--- angular-ui/src/app/modules/dashboard/map-list/map-list-service.ts
+++ angular-ui/src/app/modules/dashboard/map-list/map-list-service.ts
@@ -20,15 +20,235 @@ export class MapListService {
 			name: 'Map 2',
 			date_creation: '2021-08-06',
 			owner: 'you'
-		}
+		},
+		{
+			id: '3',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '4',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '5',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '6',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '7',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '8',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '9',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '10',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '11',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '12',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '1',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '2',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '3',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '4',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '5',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '6',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '7',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '8',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '9',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '10',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '11',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '12',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '1',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '2',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '3',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '4',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '5',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '6',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '7',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '8',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '9',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '10',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
+		{
+			id: '11',
+			name: 'Map 1',
+			date_creation: '2021-08-07',
+			owner: 'you'
+		},
+		{
+			id: '12',
+			name: 'Map 2',
+			date_creation: '2021-08-06',
+			owner: 'you'
+		},
 	];
 
 	constructor(private httpClient: HttpClient) {
 	}
 
-	getMaps(): Observable<any> {
-		let url = environment.apiUrl + "/maps";
-		return this.httpClient.get<MapData>(url);
+	getMaps(): Observable<Maps>{
+		// let url = environment.apiUrl + "/maps";
+		// return this.httpClient.get<MapData>(url);
+		return of<Maps>(
+			this.mapsInfo
+		).pipe(
+			tap((map) => {
+				console.log(map);
+			})
+		);
+	}
+
+	insertMaps(datos : any){
+		let url = environment.apiUrl + '/insert'
+		return this.httpClient.post(url,'');
+	}
+
+	deleteMaps(){
+		// insertar logica
 	}
 }
 
