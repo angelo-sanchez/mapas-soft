a14d7170db59e811f6c80a1d0e07675c325e2f1e
Nico <nicolaserrea@hotmail.com>
2022-08-08 19:48:32 -0300

    actualización de dependencias

--- service-login/src/models/user.ts
+++ service-login/src/models/user.ts
@@ -1,5 +1,6 @@
 import { model, Schema, Document } from "mongoose";
-import bcrypt from "bcrypt";
+
+const bcrypt = require("bcryptjs")
 
 export interface IUser extends Document {
   email: string;
